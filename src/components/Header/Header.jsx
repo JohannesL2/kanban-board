bb9504577bb04f9a4db2697e68579b536b15443b
JohannesL2 <johannes.lindgren2002@gmail.com>
2025-10-11 01:08:24 +0200

    Added how to use and settings button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function Header() {
   return (
+<header>
     <div>
         <h1 className='font-smooch text-5xl text-white tracking-wide drop-shadow-md'>Kanban Board</h1>
     </div>
+
+    <div className='flex items-center justify-center gap-4 mt-12'>
+        <button className='px-4 py-2 bg-black/20 text-white rounded-xl hover:bg-black/30 transition cursor-pointer'>How to use</button>
+        <button className='px-4 py-2 bg-black/20 text-white rounded-xl hover:bg-black/30 transition cursor-pointer'>Settings</button>
+    </div>
+</header>
   )
 }
