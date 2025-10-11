15c86cc769cf24940762dac4003d56eb92b512e1
JohannesL2 <johannes.lindgren2002@gmail.com>
2025-10-11 15:52:16 +0200

    Made design more mobile friendly

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -71,7 +71,7 @@ export default function List() {
   };
 
   return (
-    <div className='min-h-screen p-6'>
+    <div className='min-h-screen p-4 w-full overflow-x-hidden'>
 <div className='flex justify-center mb-10'>
     <form onSubmit={createSection} className='flex items-center gap-12  bg-white/20 p-4 rounded-2xl shadow-lg'>
       <div className="flex flex-col flex-grow gap-2">
@@ -79,7 +79,7 @@ export default function List() {
           type="text"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
-          className='bg-white/40 rounded-2xl p-4 text-2xl'
+          className='bg-white/40 rounded-2xl p-4 sm:p-4 text-lg sm:text-2xl'
           placeholder='type a title'
           />
 
@@ -101,7 +101,7 @@ export default function List() {
         items={sections.map(s => s.id)}
         strategy={verticalListSortingStrategy}
     >
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+    <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
         {sections.map((section) => (
             <Section
                 key={section.id}
