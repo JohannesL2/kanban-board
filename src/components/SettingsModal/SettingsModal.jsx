2651f027c0592cbf380a408eedcac8ec44f8b9a4
JohannesL2 <johannes.lindgren2002@gmail.com>
2025-10-17 10:28:35 +0200

    changed dark mode colors on save & close button in settingsmodal

--- src/components/SettingsModal/SettingsModal.jsx
+++ src/components/SettingsModal/SettingsModal.jsx
@@ -38,7 +38,7 @@ export default function SettingsModal({isOpen, onClose, resetBoard}) {
 
               <div className="mt-4">
                 <Button
-                  className="inline-flex items-center gap-2 rounded-md bg-gray-700 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700"
+                  className="inline-flex items-center gap-2 rounded-md bg-gray-700 dark:bg-zinc-200 dark:text-black px-3 py-1.5 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:not-data-focus:outline-none data-focus:outline data-focus:outline-white data-hover:bg-gray-600 data-open:bg-gray-700"
                   onClick={onClose}
                 >
                   Save & Close
