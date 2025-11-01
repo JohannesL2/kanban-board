19299ecee7f5572a60ff332a9b887835826a018f
JohannesL2 <johannes.lindgren2002@gmail.com>
2025-11-01 14:52:16 +0100

    smooth transition with framer motion on dark mode toggle button

--- src/components/DarkModeToggle/DarkModeToggle.jsx
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -1,5 +1,6 @@
 import { LuMoon, LuSun } from "react-icons/lu";
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 export default function DarkModeToggle() {
     const [darkMode, setDarkMode] = useState(() => {
@@ -24,11 +25,30 @@ export default function DarkModeToggle() {
     }, [darkMode])
 
   return (
-            <button 
-            className="bg-transparent p-3 hover:bg-black/10 rounded-lg cursor-pointer dark:hover:bg-zinc-100/10 dark:text-white"
-                onClick={() => setDarkMode(!darkMode)}
+            <motion.button
+            whileHover={{ scale: 1.15 }}
+            whileTap={{ scale: 0.9 }}
+            onClick={() => setDarkMode(!darkMode)}
+            className="relative 
+        flex items-center justify-center 
+        w-12 h-12 
+        rounded-full 
+        bg-gradient-to-tr from-zinc-500 to-zinc-700 
+        dark:from-yellow-500 dark:to-orange-400
+        shadow-md 
+        hover:shadow-lg 
+        transition-all duration-300 
+        text-white"
             >
+                <motion.div
+                    key={darkMode ? 'sun' : 'moon'}
+                    initial={{ rotate: -90, opacity: 0 }}
+                    animate={{ rotate: 0, opacity: 1 }}
+                    exit={{ rotate: 90, opacity: 0 }}
+                    transition={{ duration: 0.3 }}
+                >
                 {darkMode ? <LuSun className="text-2xl"/> : <LuMoon className="text-2xl" />}
-            </button>
+                </motion.div>
+            </motion.button>
   )
 }
