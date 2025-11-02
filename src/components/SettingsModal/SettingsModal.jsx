b42d67ef850b2e47b6269b45cf953ffb84c02142
JohannesL2 <johannes.lindgren2002@gmail.com>
2025-11-03 00:02:23 +0100

    Placed handleReset outside of return in settingsmodal

--- src/components/SettingsModal/SettingsModal.jsx
+++ src/components/SettingsModal/SettingsModal.jsx
@@ -2,6 +2,17 @@ import { Button, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
 import DarkModeToggle from '../DarkModeToggle/DarkModeToggle';
 
 export default function SettingsModal({isOpen, onClose, resetBoard}) {
+  const handleReset = () => {
+    if (
+      confirm(
+        "Are you sure you want to reset the board? This cannot be undone."
+      )
+    ) {
+      resetBoard();
+      onClose();
+    }
+  }
+
   return (
     <>
       <Dialog open={isOpen} onClose={onClose} className="relative z-10 focus:outline-none">
@@ -23,12 +34,7 @@ export default function SettingsModal({isOpen, onClose, resetBoard}) {
                 
                 <Button
                   className="bg-red-600 hover:bg-red-700 text-white rounded-md px-3 py-2 mt-4"
-                  onClick={() => {
-                    if (confirm("Are you sure you want to reset the board? This cannot be undone.")) {
-                      resetBoard();
-                      onClose();
-                    }
-                  }}
+                  onClick={handleReset}
                 >
                   🔄 Reset Board
                 </Button>
